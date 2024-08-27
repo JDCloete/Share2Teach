d6f02e52fffa79e0539263913f3b3386a01ca6e5
JDCloete <jacques19cloete@gmail.com>
2024-08-27 19:00:20 +0200

    Added vuetify to Laravel

---
+++ resources/js/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-card>
+        <v-card-title>
+            Hello Triple Vision
+        </v-card-title>
+        <v-card-subtitle>
+            Hello
+        </v-card-subtitle>
+        <v-card-text>
+            <v-icon>
+                mdi-human-female-girl
+            </v-icon>
+        </v-card-text>
+        <v-card-actions>
+            <v-btn>
+                Click Me ! :)
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: 'App',
+};
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import vuetify from "./vuetify";
+import { createApp } from 'vue';
+import App from './App.vue';
+
+
+createApp(App).use(vuetify).mount("#app");
