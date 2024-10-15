632a4f431bde4ff83af8b01096cf5249fb63766b
DANIKA LE ROUX <danikaleroux111@gmail.com>
2024-10-15 15:51:54 +0200

    Worked on about us page

--- resources/js/Pages/AboutUsPage.vue
+++ resources/js/Pages/AboutUsPage.vue
@@ -1,13 +1,316 @@
 <template>
-    <v-card>
-        <v-card-title>
-            Hello AboutUsPage
-        </v-card-title>
+    <v-card class="background-color">
+        <!-- About Us Header with Background Image -->
+        <div class="d-flex justify-center align-center header-background" style="height: 40vh; background-size: cover; background-position: center;">
+            <div class="mb-6" style="text-align: center;">
+                <h1 class="text-h1 custom-font" style="color: #000000;">
+                    About Us
+                </h1>
+                <p class="text-h6" style="color: #000000;">
+                    Share2Teach is a platform where you can share your knowledge and learn from others.
+                </p>
+            </div>
+        </div>
+
+        <!-- Introduction Section -->
+        <v-card elevation="6" class="mb-6 mission-container">
+            <v-card class="pa-6" elevation="2">
+                <v-card-title class="text-h4 text-center">
+                    Our Mission
+                </v-card-title>
+                <v-card-text>
+                    <p class="paragraph-spacing paragraph-text">
+                        Our primary objective is to create a teaching and learning environment that is as seamless, integrated, and user-friendly as possible. We are fully committed to developing a comprehensive and multifaceted platform that not only facilitates the efficient sharing of resources but also fosters meaningful connections among educators and actively engages students in the learning process. Share2Teach stands as an innovative solution designed specifically for educators, empowering them to collaborate effectively and efficiently, regardless of their teaching context.
+                    </p>
+                    <p class="paragraph-spacing paragraph-text">
+                        In our pursuit of this mission, we strive to provide a wide array of tools and functionalities that simplify the often complex and demanding process of lesson planning. By doing so, we enable teachers to redirect their focus to what truly matters—their students and the impact they have on their learning journeys. Our platform further empowers educators by offering the capability to track student progress in real-time, allowing them to identify areas that may require additional support and to celebrate the achievements and milestones their students reach along the way.
+                    </p>
+                    <p class="paragraph-spacing paragraph-text">
+                        Moreover, Share2Teach streamlines the management of educational content, making it significantly easier for teachers to curate, organize, and distribute high-quality materials that enhance the overall learning experience. By simplifying these processes, we hope to alleviate some of the burdens educators face, allowing them to dedicate more time and energy to nurturing their students’ growth and success.
+                    </p>
+                </v-card-text>
+            </v-card>
+        </v-card>
+
+
+        <!-- Features Section -->
+        <v-card elevation="6" class="mb-6 mission-container">
+            <v-card class="pa-6" elevation="2">
+                <v-card-title class="text-h4 text-center">
+                    Key Features
+                </v-card-title>
+                <div class="d-flex justify-center align-center">
+                    <v-list>
+                        <v-list-item>
+                            <v-list-item-icon class="mr-3">
+                                <v-icon color="purple">mdi-account-group</v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-content>
+                                Collaborative environment for educators and students
+                            </v-list-item-content>
+                        </v-list-item>
+
+                        <v-list-item>
+                            <v-list-item-icon class="mr-3">
+                                <v-icon color="purple">mdi-cloud-upload</v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-content>
+                                Upload and share educational resources
+                            </v-list-item-content>
+                        </v-list-item>
+
+                        <v-list-item>
+                            <v-list-item-icon class="mr-3">
+                                <v-icon color="purple">mdi-check-circle</v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-content>
+                                Facilitate interactive learning experiences
+                            </v-list-item-content>
+                        </v-list-item>
+
+                        <v-list-item>
+                            <v-list-item-icon class="mr-3">
+                                <v-icon color="purple">mdi-school</v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-content>
+                                Accessible for both educators and students
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list>
+                </div>
+            </v-card>
+        </v-card>
+
+
+        <v-card elevation="6" class="mb-6 mission-container">
+            <v-card class="pa-6" elevation="2">
+                <v-card-title class="text-h4 text-center">
+                    Our Core Values
+                </v-card-title>
+                <v-card-text >
+                    <v-list>
+                        <v-list-item>
+                            <v-row no-gutters>
+                                <v-col class="d-flex justify-center" cols="1">
+                                    <v-icon color="purple">mdi-heart</v-icon>
+                                </v-col>
+                                <v-col class="pl-2" cols="11">
+                                    <v-list-item-content>
+                                        Passion for Education – At the heart of everything we do is a deep commitment to education. We firmly believe in its transformative power to shape lives, empower individuals, and pave the way for a brighter and more promising future. Our passion drives us to support learners and educators alike, creating opportunities for growth and success through meaningful educational experiences.
+                                    </v-list-item-content>
+                                </v-col>
+                            </v-row>
+                        </v-list-item>
+
+                        <v-list-item>
+                            <v-row no-gutters>
+                                <v-col class="d-flex justify-center" cols="1">
+                                    <v-icon color="purple">mdi-handshake</v-icon>
+                                </v-col>
+                                <v-col class="pl-2" cols="11">
+                                    <v-list-item-content>
+                                        Collaboration – We are dedicated to fostering a vibrant and inclusive community where educators, students, and stakeholders come together to exchange knowledge, share ideas, and collaborate toward a common goal. By working together, we create a dynamic environment where learning thrives, and everyone can grow both personally and professionally.
+                                    </v-list-item-content>
+                                </v-col>
+                            </v-row>
+                        </v-list-item>
+
+                        <v-list-item>
+                            <v-row no-gutters>
+                                <v-col class="d-flex justify-center" cols="1">
+                                    <v-icon color="purple">mdi-lightbulb-on</v-icon>
+                                </v-col>
+                                <v-col class="pl-2" cols="11">
+                                    <v-list-item-content>
+                                        Innovation – We are continually pushing the boundaries of traditional learning by developing and offering innovative tools and resources that elevate the teaching and learning experience. Our aim is to make education more engaging, interactive, and effective, ensuring that learners and educators have access to the most cutting-edge solutions to achieve their goals.
+                                    </v-list-item-content>
+                                </v-col>
+                            </v-row>
+                        </v-list-item>
+                    </v-list>
+                </v-card-text>
+            </v-card>
+        </v-card>
+
+        <!-- Goals Section -->
+        <v-card elevation="6" class="mb-6 mission-container">
+            <v-card class="pa-6" elevation="2">
+                <v-card-title class="text-h4 text-center">
+                    Our Goals
+                </v-card-title>
+                <v-card-text>
+                    <p class="paragraph-spacing text-h6.5">
+                        Our foremost objective is to profoundly enhance the overall quality of education through the cultivation of a collaborative spirit among educators. We recognize the immense value of fostering connections within the educational community, empowering teachers to gain access to a wide-ranging array of resources generously shared by their peers. This approach not only enriches the professional development of educators but also contributes significantly to the improvement of educational outcomes for students.
+                    </p>
+                    <p class="paragraph-spacing text-h6.5">
+                        By establishing this interconnected framework, we aim to facilitate the seamless exchange of innovative ideas, effective strategies, and diverse teaching materials among educators. This collaborative effort will create a vibrant and dynamic ecosystem in which educators can actively support one another, share their expertise, and collectively work toward elevating the educational experience for their students.
+                    </p> <p class="paragraph-spacing text-h6.5">
+                    In fostering such an environment, we aspire to cultivate a thriving learning community characterized by shared knowledge, mutual encouragement, and professional camaraderie. Ultimately, our goal is to contribute to the continuous advancement of education as a whole, ensuring that both educators and students benefit from the collective wisdom and resources available within this supportive network.
+                    </p>
+                </v-card-text>
+            </v-card>
+        </v-card>
+
+        <!-- Contact Us Section -->
+        <v-card elevation="2" class="pa-6 my-6">
+            <v-card-title class="text-h5 text-center">
+                Contact Us
+            </v-card-title>
+            <v-card-text class="text-center text-h6">
+                Have questions or need help? Reach out to us!
+            </v-card-text>
+
+            <!-- Add Contact People Section -->
+            <div class="d-flex justify-center align-center flex-wrap">
+                <!-- Person 1 -->
+                <v-card max-width="300" class="mx-4 my-4 contact-card" elevation="2">
+                    <a :href="'mailto:40779173@mynwu.ac.za'">
+                        <v-img
+                            src="Jeanluc.jpg"
+                            class="contact-image"
+                            height="300px" ><!-- Increase the height -->
+                        </v-img>
+                    </a>
+                    <v-card-title class="text-center mb-1"> <!-- Added mb-1 class for margin bottom -->
+                        Jean-luc Begue
+                    </v-card-title>
+                    <v-card-subtitle class="text-center">
+                        Mobile: 078-023-6331<br>
+                        Email: 40779173@mynwu.ac.za
+                    </v-card-subtitle>
+                </v-card>
+
+                <!-- Person 2 -->
+                <v-card max-width="300" class="mx-4 my-4 contact-card" elevation="2">
+                    <a :href="'mailto:41049764@mynwu.ac.za'">
+                        <v-img
+                            src="Danika.jpg"
+                            class="contact-image"
+                            height="300px" ><!-- Increase the height -->
+                        </v-img>
+                    </a>
+                    <v-card-title class="text-center mb-1"> <!-- Added mb-1 class for margin bottom -->
+                        Danika le Roux
+                    </v-card-title>
+                    <v-card-subtitle class="text-center">
+                        Mobile: 076-913-5006<br>
+                        Email: 41049764@mynwu.ac.za
+                    </v-card-subtitle>
+                </v-card>
+
+                <!-- Person 3 -->
+                <v-card max-width="300" class="mx-4 my-4 contact-card" elevation="2">
+                    <a :href="'mailto:44214987@mynwu.ac.za'">
+                        <v-img
+                            src="Jacques.jpg"
+                            class="contact-image"
+                            height="300px" > <!-- Increase the height -->
+                        </v-img>
+                    </a>
+                    <v-card-title class="text-center mb-1"> <!-- Added mb-1 class for margin bottom -->
+                        Jacques Cloete
+                    </v-card-title>
+                    <v-card-subtitle class="text-center">
+                        Mobile: 060-471-4349<br>
+                        Email: 44214987@mynwu.ac.za
+                    </v-card-subtitle>
+                </v-card>
+            </div>
+
+            <!-- Contact Button -->
+            <!-- <div class="d-flex justify-center"> -->
+            <!--     <v-btn color="purple" href="/contact" class="text-white"> -->
+            <!--         Get in Touch -->
+            <!--     </v-btn> -->
+            <!-- </div> -->
+
+        </v-card>
     </v-card>
 </template>
 
-<script>
-export default {
-    name: 'AboutUsPage',
-};
-</script>
+
+<style scoped>
+.background-color {
+    background-color: #f9f9f9;
+    min-height: 100vh;
+    width: 100%;
+}
+
+.pa-6 {
+    padding: 24px;
+}
+
+.mb-6 {
+    margin-bottom: 24px;
+}
+
+.my-6 {
+    margin-top: 24px;
+    margin-bottom: 24px;
+}
+
+.paragraph-spacing {
+    margin-bottom: 16px;
+}
+.paragraph-text {
+    font-size: 1.25rem; /* Adjust this value as needed */
+    line-height: 1.5; /* Optional: Adjust line height for readability */
+}
+
+.custom-font {
+    font-family: 'Merriweather', serif;
+    font-size: 2.5rem;
+    color: #4A4A4A;
+}
+
+.text-h4 {
+    font-family: 'Lora', serif; /* Use Lora font */
+    font-weight: bold;
+    color: #333;
+    text-decoration: underline; /* Add this line to underline the text */
+}
+
+.text-h5 {
+    font-family: 'Lora', serif;
+    font-weight: bold;
+    color: #6A1B9A;
+    text-decoration: underline; /* Add this line to underline the text */
+}
+
+/* Parallax header background */
+.header-background {
+    background-image: url('https://as2.ftcdn.net/v2/jpg/03/57/05/61/1000_F_357056172_AOxoyKV4D20Bsw17SvkzcMfWSOLTIGzJ.jpg');
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed; /* Fixed for parallax effect */
+}
+
+.contact-card {
+    width: 300px;
+    height: 400px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    transition: transform 0.3s ease, box-shadow 0.3s ease; /* Adds transition for smooth effect */
+}
+
+.contact-card:hover {
+    transform: scale(1.05); /* Slightly increases the size on hover */
+    box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2); /* Adds a shadow effect on hover */
+}
+
+.contact-image {
+    object-fit: cover;
+}
+
+.mission-container {
+    max-width: 1400px; /* Adjust the width to your preference */
+    margin: 0 auto; /* Centers the card on the page */
+    transition: transform 0.3s ease, box-shadow 0.3s ease; /* Adds transition for smooth effect */
+}
+
+.mission-container:hover {
+    transform: scale(1.05); /* Slightly increases the size on hover */
+    box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2); /* Adds a shadow effect on hover */
+}
+</style>
